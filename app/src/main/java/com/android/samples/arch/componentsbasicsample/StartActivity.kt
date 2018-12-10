606ab280a7223231b7d87ef933e35ff400d8ef4d
Yuvraj <uvasharma@gmail.com>
2018-12-10 23:22:43 +0530

    Toast added

--- app/src/main/java/com/android/samples/arch/componentsbasicsample/StartActivity.kt
+++ app/src/main/java/com/android/samples/arch/componentsbasicsample/StartActivity.kt
@@ -2,11 +2,13 @@ package com.android.samples.arch.componentsbasicsample
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 
 class StartActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+        Toast.makeText(this,"added",Toast.LENGTH_SHORT).show()
     }
 }
